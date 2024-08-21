da3d2c64a98b289ddfd42a02e276a717c98faa1d
Urs Joss <urs.joss@gmx.ch>
2024-08-21 22:00:34 +0200

    migrate au.com.trgtd.tr.prefs.dates

---
+++ modules/au.com.trgtd.tr.prefs.dates/build.gradle.kts
@@ -0,0 +1,9 @@
+description = "TR-Prefs-Dates"
+
+dependencies {
+    implementation(project(":au.com.trgtd.tr.appl"))
+    implementation(libs.netbeans.openide.modules.options.api)
+    implementation(libs.netbeans.openide.util)
+    implementation(libs.miglayout.swing)
+}
+
