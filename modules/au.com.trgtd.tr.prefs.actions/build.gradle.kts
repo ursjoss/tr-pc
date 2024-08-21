a8c63fcafc2704ef6655466dc2c62c2f72d292ae
Urs Joss <urs.joss@gmx.ch>
2024-08-21 23:04:16 +0200

    migrate au.com.trgtd.tr.prefs.actions

---
+++ modules/au.com.trgtd.tr.prefs.actions/build.gradle.kts
@@ -0,0 +1,12 @@
+description = "TR-Prefs-Actions"
+
+dependencies {
+    implementation(project(":au.com.trgtd.tr.appl"))
+    implementation(project(":au.com.trgtd.tr.swing"))
+    implementation(project(":au.com.trgtd.tr.util"))
+
+    implementation(libs.netbeans.openide.modules.options.api)
+    implementation(libs.netbeans.openide.util.ui)
+    implementation(libs.miglayout.swing)
+}
+
