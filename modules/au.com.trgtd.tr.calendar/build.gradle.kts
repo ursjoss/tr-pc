7725f20875e92e47e90e334707c3e9cee16c4932
Urs Joss <urs.joss@gmx.ch>
2024-08-21 22:19:05 +0200

    migrate au.com.trgtd.tr.calendar

---
+++ modules/au.com.trgtd.tr.calendar/build.gradle.kts
@@ -0,0 +1,10 @@
+description = "TR-Calendar"
+
+dependencies {
+    implementation(project(":au.com.trgtd.tr.appl"))
+    implementation(project(":au.com.trgtd.tr.swing"))
+
+    implementation(libs.netbeans.openide.modules.options.api)
+    implementation(libs.miglayout.swing)
+}
+
