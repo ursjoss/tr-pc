2e0b346e0144bc9cd1f1af92eedbceffab998611
Urs Joss <urs.joss@gmx.ch>
2024-08-26 22:49:37 +0200

    au.com.trgtd.tr.prefs.recurrence: separate resources and declare dep as api

--- modules/au.com.trgtd.tr.prefs.recurrence/build.gradle.kts
+++ modules/au.com.trgtd.tr.prefs.recurrence/build.gradle.kts
@@ -5,9 +5,11 @@ plugins {
 }
 
 dependencies {
+    api(libs.netbeans.openide.modules.options.api)
+    api(libs.netbeans.openide.util)
+
     implementation(project(":au.com.trgtd.tr.appl"))
 
-    implementation(libs.netbeans.openide.util)
     implementation(libs.miglayout.swing)
 }
 
