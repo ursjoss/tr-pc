9e1a0c533ae732d35901778388f8d7b39fe42880
Urs Joss <urs.joss@gmx.ch>
2024-08-26 17:21:00 +0200

    Migrate au.com.trgtd.tr.prefs.data

---
+++ modules/au.com.trgtd.tr.prefs.data/build.gradle.kts
@@ -0,0 +1,13 @@
+description = "TR-Prefs-Data"
+
+dependencies {
+    implementation(project(":au.com.trgtd.tr.appl"))
+    implementation(project(":au.com.trgtd.tr.swing"))
+    implementation(project(":au.com.trgtd.tr.util"))
+
+    implementation(libs.netbeans.openide.modules.options.api)
+    implementation(libs.netbeans.openide.util)
+    implementation(libs.netbeans.openide.windows)
+    implementation(libs.miglayout.swing)
+}
+
