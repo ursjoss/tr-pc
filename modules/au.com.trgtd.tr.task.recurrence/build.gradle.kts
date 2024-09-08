a2928a218d2d4d56d6071351d1b3652a8992919f
Urs Joss <urs.joss@gmx.ch>
2024-09-08 18:34:05 +0200

    au.com.trgtd.tr.task.recurrence

---
+++ modules/au.com.trgtd.tr.task.recurrence/build.gradle.kts
@@ -0,0 +1,13 @@
+description = "TR-Task-Recurrence"
+
+plugins {
+    id("java-library")
+}
+
+dependencies {
+    api(libs.netbeans.openide.modules.options.api)
+
+    implementation(project(":au.com.trgtd.tr.appl"))
+    implementation(project(":tr.model"))
+}
+
