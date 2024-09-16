2b2141a31a963b4b4c841d93ccaf26524977d3b6
Urs Joss <urs.joss@gmx.ch>
2024-09-16 13:58:09 +0200

    au.com.trgtd.tr.reports.actions.scheduled

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,6 +38,7 @@ val auTrProjects = listOf(
     "util",
     "report.project.detail",
     "report.project.outline",
+    "reports.actions.scheduled",
     "reports.done",
     "reports.pocketmod",
     "reports.reference",
