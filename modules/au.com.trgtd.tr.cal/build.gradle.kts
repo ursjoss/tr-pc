31cb0c5e51beae84f937049aa8f881810404b94b
Urs Joss <urs.joss@gmx.ch>
2024-09-16 13:20:27 +0200

    fix au.com.trgtd.tr.cal

--- modules/au.com.trgtd.tr.cal/build.gradle.kts
+++ modules/au.com.trgtd.tr.cal/build.gradle.kts
@@ -8,5 +8,8 @@ dependencies {
     implementation(project(":au.com.trgtd.tr.appl"))
     implementation(project(":au.com.trgtd.tr.prefs.dates"))
     implementation(project(":au.com.trgtd.tr.services"))
+    implementation(project(":tr.model"))
+
+    implementation(libs.miglayout.swing)
 }
 
