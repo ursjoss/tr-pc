6614285421b8fd6977af15a39a8016f3f01b1eab
Urs Joss <urs.joss@gmx.ch>
2024-09-17 14:44:52 +0200

    au.com.trgtd.tr.l10n.es_ES

---
+++ modules/au.com.trgtd.tr.l10n.es_ES/build.gradle.kts
@@ -0,0 +1,11 @@
+description = "TR-l10n-es_ES"
+
+plugins {
+    id("java-library")
+}
+
+dependencies {
+    implementation(project(":au.com.trgtd.tr.appl"))
+    implementation(project(":au.com.trgtd.tr.view.overview"))
+}
+
