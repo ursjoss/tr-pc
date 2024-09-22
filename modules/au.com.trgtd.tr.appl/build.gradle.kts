c063eb19caf72101a6d88373e6be8abf9a96b052
Urs Joss <urs.joss@gmx.ch>
2024-09-22 15:08:25 +0200

    Allow to launch empty netbeans window

--- modules/au.com.trgtd.tr.appl/build.gradle.kts
+++ modules/au.com.trgtd.tr.appl/build.gradle.kts
@@ -7,19 +7,10 @@ plugins {
 dependencies {
     api(libs.netbeans.modules.options.api)
 
-    implementation(libs.netbeans.openide.modules)
-    implementation(libs.netbeans.openide.util)
-    implementation(libs.netbeans.openide.util.lookup)
-    implementation(libs.netbeans.openide.windows)
-    implementation(libs.netbeans.modules.bootstrap)
-    implementation(libs.netbeans.modules.projectapi)
-    implementation(libs.netbeans.modules.projectapi.nb)
-    implementation(libs.netbeans.api.annotations.common)
-    implementation(libs.netbeans.modules.core.multitabs.project)
-    implementation(libs.netbeans.modules.core.windows)
-    implementation(libs.netbeans.modules.javahelp)
-    implementation(libs.netbeans.swing.tabcontrol)
-    implementation(libs.netbeans.jdesktop.layout)
     implementation(libs.miglayout.swing)
+
+    runtimeOnly(libs.netbeans.modules.projectapi.nb)
+    runtimeOnly(libs.netbeans.modules.core.multitabs.project)
+    runtimeOnly(libs.netbeans.modules.core.windows)
 }
 
